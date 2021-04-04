127360fd5385e91e7130050dd83d87d297567b5a
jonas-colas <jonas.colas@yahoo.com>
2021-04-04 03:55:41 -0300

    change from TypeScript to JavaScript

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React , {Fragment} from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Login from './pages/Login'
+
+const App = () => {
+  return (
+    
+  );
+}
+
+export default App;
